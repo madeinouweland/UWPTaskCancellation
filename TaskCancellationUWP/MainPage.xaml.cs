101eea4c07c85e9dbbe44eff0cece2b40f117e80
madeinouweland <loek2015@outlook.com>
2016-10-02 09:51:59 +0200

    go

---
+++ TaskCancellationUWP/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TaskCancellationUWP
+{
+    public sealed partial class MainPage : Page
+    {
+        private CancellationTokenSource _cancel;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async Task Foo(CancellationTokenSource cancel)
+        {
+            while (!cancel.IsCancellationRequested)
+            {
+                info.Text = DateTime.Now.Ticks + "";
+                await Task.Delay(2000);
+            }
+            info.Text = "cancelled";
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            info.Text = "starting task";
+
+            _cancel = new CancellationTokenSource();
+            await Foo(_cancel);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            info.Text = "cancelling task";
+            _cancel.Cancel();
+        }
+    }
+
+}
